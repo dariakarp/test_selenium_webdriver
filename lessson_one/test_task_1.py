025596604a7bd5413c87c60569e994f1055372b6
Дарья <dkitaly26@gmail.com>
2024-02-29 11:18:44 +0300

    Create test_task_1.py
    Открытие браузера, ввод в строку поиска "Hello, world!". Клик по кнопке "Поиск в Google"

---
+++ lessson_one/test_task_1.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_open_page(driver):
+    driver.get("https://www.google.com/")
+    driver.find_element("name", "q").send_keys("Hello, world!")
+    time.sleep(3)
+    driver.find_element("name", "btnK").click()
