b7f76426885e3b82c8d46cc9d4bdbeb8dfd22072
Дарья <dkitaly26@gmail.com>
2024-03-23 22:42:35 +0300

    Задание 13. Сценарий работы с корзиной.
    ) открыть главную страницу
    2) открыть первый товар из списка
    2) добавить его в корзину (при этом может случайно добавиться товар, который там уже есть, ничего страшного)
    3) подождать, пока счётчик товаров в корзине обновится
    4) вернуться на главную страницу, повторить предыдущие шаги ещё два раза, чтобы в общей сложности в корзине было 3 единицы товара
    5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
    6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица

---
+++ lesson_seven/test_task13.py
@@ -0,0 +1,62 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+
+import pytest
+import os
+from selenium import webdriver
+from faker import Faker
+fake = Faker()
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    wd.implicitly_wait(10)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_work_in_basket(driver):
+    wait = WebDriverWait(driver, 10)
+    driver.get("http://localhost/litecart/en/")
+    product_in_box = 0
+    for i in range(3):
+        driver.find_elements("xpath", "//li[@class= 'product column shadow hover-light']")
+        driver.find_element("xpath", "//div[@class= 'image-wrapper']").click()
+        try:
+            driver.find_element("xpath", "//select[@name= 'options[Size]']")
+            driver.find_element("xpath", "//select[@name= 'options[Size]']").click()
+            driver.find_element("xpath", "//select[@name= 'options[Size]']/option[3]").click()
+            driver.find_element("xpath", "//button[@name = 'add_cart_product']").click()
+        except NoSuchElementException:
+            print("нет ошибки")
+            driver.find_element("xpath", "//button[@name = 'add_cart_product']").click()
+
+        product_in_box += 1
+        count_wait = str(product_in_box)
+        wait.until(EC.text_to_be_present_in_element(("xpath", "//div[@id = 'cart']//span[@class = 'quantity']"), count_wait))
+        driver.back()
+    driver.find_element("xpath", "//div[@id ='cart']/a[3]").click() #клик на "Checkout" на корзину
+    products = driver.find_elements("xpath", "//form[@name = 'cart_form']/div//a")#нахожу все товары в таблице
+
+    for j in range(len(products)):
+        driver.find_element("xpath", "//button[@name =  'remove_cart_item' ]").click() #удаляю товар
+        table = driver.find_element("xpath", "//div[@id = 'box-checkout-summary']") #локатор таблицы
+        wait.until(EC.staleness_of(table)) #жду пока талица обновится
+
+
+
+
+
+
+
+
+
+
+
+
+
+
