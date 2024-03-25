489f6fce24cc0fa89cc0542058106c685178ef00
Дарья <dkitaly26@gmail.com>
2024-03-25 14:20:46 +0300

    ссылки открываются в новом окне
    Сценарий должен состоять из следующих частей:
    
    1) зайти в админку
    2) открыть пункт меню Countries (или страницу http://localhost/litecart/admin/?app=countries&doc=countries)
    3) открыть на редактирование какую-нибудь страну или начать создание новой
    4) возле некоторых полей есть ссылки с иконкой в виде квадратика со стрелкой -- они ведут на внешние страницы и открываются в новом окне, именно это и нужно проверить.
    
    Требуется именно кликнуть по ссылке, чтобы она открылась в новом окне, потом переключиться в новое окно, закрыть его, вернуться обратно, и повторить эти действия для всех таких ссылок.

---
+++ lesson_eight/test_task14.py
@@ -0,0 +1,42 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    wd.implicitly_wait(10)
+    request.addfinalizer(wd.quit)
+    return wd
+def test_click_on_all_links(driver):
+    wait = WebDriverWait(driver, 10)
+    driver.get("http://localhost/litecart/admin") #вход в админку
+    driver.find_element("name", "username").send_keys("admin")
+    driver.find_element("name", "password").send_keys("admin")
+    driver.find_element("name", "login").click()
+
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries") #вход на страницу со странами
+    driver.find_element("xpath", "//a[@class = 'button']").click()#клик на кнопку "добавить новую страну"
+
+    old_windows = driver.window_handles #получаю список окон (пока только одно окно)
+    all_links = driver.find_elements("xpath", "//td[@id = 'content']//td//a[not(@id = 'address-format-hint')]")#нахожу все ссылки, на которые нужно кликнуть
+    for i in range(len(all_links)):
+        all_links = driver.find_elements("xpath","//td[@id = 'content']//td//a[not(@id = 'address-format-hint')]")  # нахожу все ссылки, на которые нужно кликнуть
+        links = all_links[i]
+        links.click()#кликаю на иконку для открытия нового окна
+        all_windows = driver.window_handles #получаю список всех окон
+        new_windows = [x for x in all_windows if x not in old_windows]#получаю второе окно
+        driver.switch_to.window(new_windows[0])#переключаюсь в новое окно
+        driver.close()
+        driver.switch_to.window(old_windows[0])#переключаюсь в первое (изначальное) окно
+
+
+
+
+
+
+
+
+
