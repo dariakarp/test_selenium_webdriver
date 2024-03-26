802eb4849511b70c29a55a6c05d2d3d9d537bae5
Дарья <dkitaly26@gmail.com>
2024-03-26 22:48:24 +0300

    Update test_task10_Chrome.py

--- lesson_five/test_task10_Chrome.py
+++ lesson_five/test_task10_Chrome.py
@@ -23,11 +23,6 @@ def test_name_in_main_page_and_name_in_product(driver):
     assert name_in_main_page == name_in_product
 
 
-    time.sleep(4)
-
-
-
-
 
 def test_price_in_main_page_and_name_in_product(driver):
     driver.get("http://localhost/litecart/en/")
@@ -46,37 +41,26 @@ def test_price(driver): #проверка, что у первого товара
     regular_price = driver.find_element("xpath","//div[@id = 'box-campaigns']//s[@class='regular-price']").value_of_css_property("color") #Получаю цвет значения цены в формате RGBA
     text_decoration = driver.find_element("xpath", "//div[@id = 'box-campaigns']//s[@class='regular-price']").value_of_css_property("text-decoration")#Получаю значение line-through solid rgb(119, 119, 119), которое соответствует перечеркнутому значению
 
-    # a = list(regular_price) #значения rgba превращаю в кортеж
-    # r = a[5]+a[6]+a[7]
-    # g = a[10]+a[11]+a[12]
-    # b = a[15] + a[16] + a[17]
+    color = Color.from_string(regular_price)
+    assert color.red == color.green == color.blue# серый цвет это когда rgb совпадают
 
-    # assert r == g == b # серый цвет это когда rgb совпадают
-    assert Color.from_string('rgb(0, 0, 0)')
+
 def test_color_in_Campaigns(driver): #проверка, что цена жирная и красная на странице в блоке Campaigns
     driver.get("http://localhost/litecart/en/")
     campaign_price = driver.find_element("xpath", "//div[@id = 'box-campaigns']//strong[@class='campaign-price']").value_of_css_property('color')
     campaign_price_font_weight = driver.find_element("xpath", "//div[@id = 'box-campaigns']//strong[@class='campaign-price']").value_of_css_property('font-weight')
-
-    # a = list(campaign_price)
-    # g = int(a[10])
-    # b = int(a[13])
-    assert Color.from_string('rgb(204, 0, 0)')
-    #assert g == b == 0 # красный цвет, если g и b равны 0
+    color = Color.from_string(campaign_price)
+    assert color.green == color.blue # красный цвет, если g и b равны 0
     assert int(campaign_price_font_weight) > 699 #жирными считаются значения больше или равны 700
 
 
-def test_color_in_Campaigns(driver): #проверка, что цена жирная и красная на странице товара
+def test_color_in_product(driver): #проверка, что цена жирная и красная на странице товара
     driver.get("http://localhost/litecart/en/")
     driver.find_element("xpath", "//div[@id = 'box-campaigns']//div[@class = 'name']").click()
     campaign_price = driver.find_element("xpath","//div[@id= 'box-product']//strong[@class = 'campaign-price']").value_of_css_property('color')
     campaign_price_font_weight = driver.find_element("xpath","//div[@id= 'box-product']//strong[@class = 'campaign-price']").value_of_css_property('font-weight')
-    assert Color.from_string('rgb(204, 0, 0)')
-    # # print(campaign_price)
-    # # a = list(campaign_price)
-    # # g = int(a[10])
-    # # b = int(a[13])
-    # # assert g == b == 0  # красный цвет, если g и b равны 0
+    color = Color.from_string(campaign_price)
+    assert color.green == color.blue  # красный цвет, если g и b равны 0
     assert int(campaign_price_font_weight) > 699  # жирными считаются значения больше или равны 700
 
 
@@ -84,26 +68,20 @@ def test_campaign_price_more_than_regular_price(driver): #проверка, чт
     driver.get("http://localhost/litecart/en/")
     campaign_price = driver.find_element("xpath","//div[@id = 'box-campaigns']//strong[@class = 'campaign-price']").value_of_css_property("font-size") #размер шрифта акционной цены
     regular_price = driver.find_element("xpath","//div[@id = 'box-campaigns']//s[@class='regular-price']").value_of_css_property("font-size")
-    a = list(campaign_price)
-    b = list(regular_price)
-    size_campaign_price = a[0] +a[1]
-    size_regular_price = b[0] + b[1] + b[2] + b[3]
+    size_campaign_price = campaign_price.replace("px", "")
+    size_regular_price = regular_price.replace("px", "")
 
-    assert float(size_campaign_price) > float(size_regular_price)
+    assert size_campaign_price > size_regular_price
 
 def test_campaign_price_more_than_regular_price_in_product(driver): #проверка, что в карточке акционная цена больше, чем обычная
     driver.get("http://localhost/litecart/en/")
     driver.find_element("xpath", "//div[@id = 'box-campaigns']//div[@class = 'name']").click()
     campaign_price = driver.find_element("xpath","//div[@id= 'box-product']//strong[@class = 'campaign-price']").value_of_css_property("font-size") #размер шрифта акционной цены
     regular_price = driver.find_element("xpath","//div[@id= 'box-product']//s[@class = 'regular-price']").value_of_css_property("font-size")
-    a = list(campaign_price)
-    b = list(regular_price)
 
+    size_campaign_price = campaign_price.replace("px", "")
+    size_regular_price = regular_price.replace("px", "")
 
-    size_campaign_price = a[0] +a[1]
-    size_regular_price = b[0] + b[1]
-
-    assert float(size_campaign_price) > float(size_regular_price)
-
+    assert size_campaign_price > size_regular_price
 
 
